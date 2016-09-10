67961572209aa6d5b113a572f6ff62b5edbfc614
Interference Security <interference-security@users.noreply.github.com>
2016-09-10 13:57:36 -0400

    Created file

---
+++ find-classes.js
@@ -0,0 +1,16 @@
+console.log("[*] Started: Find Classes")
+if (ObjC.available)
+{
+    for (var className in ObjC.classes)
+    {
+        if (ObjC.classes.hasOwnProperty(className))
+        {
+            console.log(className);
+        }
+    }
+}
+else
+{
+    console.log("Objective-C Runtime is not available!");
+}
+console.log("[*] Completed: Find Classes")

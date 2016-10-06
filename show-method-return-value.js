12ef25ffdee620998fe037257c6f6a3b532a929e
Interference Security <interference-security@users.noreply.github.com>
2016-10-06 16:06:46 +0530

    Added script

---
+++ show-method-return-value.js
@@ -0,0 +1,28 @@
+if (ObjC.available)
+{
+    try
+    {
+        //Your class name here
+        var className = "YOUR_CLASS_NAME_HERE";
+        //Your function name here
+        var funcName = "YOUR_EXACT_FUNC_NAME_HERE";
+        var hook = eval('ObjC.classes.' + className + '["' + funcName + '"]');
+        Interceptor.attach(hook.implementation, {
+          onLeave: function(retval) {
+            console.log("[*] Class Name: " + className);
+            console.log("[*] Method Name: " + funcName);
+            console.log("\t[-] Type of return value: " + typeof retval);
+            //console.log(retval.toString());
+            console.log("\t[-] Return Value: " + retval);
+          }
+        });
+    }
+    catch(err)
+    {
+        console.log("[!] Exception2: " + err.message);
+    }
+}
+else
+{
+    console.log("Objective-C Runtime is not available!");
+}

538e90213da9a89f812a2f4a5bbbe0a58522433f
Interference Security <interference-security@users.noreply.github.com>
2016-10-06 15:46:47 +0530

    Added script

---
+++ show-all-methods-of-specific-class.js
@@ -0,0 +1,30 @@
+console.log("[*] Started: Find All Methods of a Specific Class");
+if (ObjC.available)
+{
+    try
+    {
+        //Your class name here
+        var className = "YOUR_CLASS_NAME_HERE";
+        var methods = eval('ObjC.classes.' + className + '.$methods');
+        for (var i = 0; i < methods.length; i++)
+        {
+            try
+            {
+                console.log("[-] "+methods[i]);
+            }
+            catch(err)
+            {
+                console.log("[!] Exception1: " + err.message);
+            }
+        }
+    }
+    catch(err)
+    {
+        console.log("[!] Exception2: " + err.message);
+    }
+}
+else
+{
+    console.log("Objective-C Runtime is not available!");
+}
+console.log("[*] Completed: Find All Methods of a Specific Class");

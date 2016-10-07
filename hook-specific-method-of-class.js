2bee681364163c3dc2dc28a7f645b0fb3636a888
Interference Security <interference-security@users.noreply.github.com>
2016-10-07 14:05:32 +0530

    Added script

---
+++ hook-specific-method-of-class.js
@@ -0,0 +1,32 @@
+//Source: http://www.mopsled.com/2015/log-ios-method-arguments-with-frida/
+if (ObjC.available)
+{
+    try
+    {
+        //Your class name here
+        var className = "YOUR_CLASS_NAME_HERE";
+        //Your function name here
+        var funcName = "YOUR_FUNC_NAME_HERE";
+        var hook = eval('ObjC.classes.' + className + '["' + funcName + '"]');
+        Interceptor.attach(hook.implementation, {
+          onEnter: function(args) {
+            // args[0] is self
+            // args[1] is selector (SEL "sendMessageWithText:")
+            // args[2] holds the first function argument, an NSString
+            console.log("[*] Detected call to: " + className + " -> " + funcName);
+            //For viewing and manipulating arguments
+            //console.log("\t[-] Value1: "+ObjC.Object(args[2]));
+            //console.log("\t[-] Value2: "+(ObjC.Object(args[2])).toString());
+            //console.log(args[2]);
+          }
+        });
+    }
+    catch(err)
+    {
+        console.log("[!] Exception2: " + err.message);
+    }
+}
+else
+{
+    console.log("Objective-C Runtime is not available!");
+}

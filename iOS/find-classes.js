f5dfe7d344f648a1d2d71f3219dd37a21346f584
int3rf3r3nc3 <interference-security@users.noreply.github.com>
2019-06-15 20:58:59 +0530

    Show number of classes found

--- iOS/find-classes.js
+++ iOS/find-classes.js
@@ -1,16 +1,18 @@
 console.log("[*] Started: Find Classes")
 if (ObjC.available)
 {
+    count = 0
     for (var className in ObjC.classes)
     {
         if (ObjC.classes.hasOwnProperty(className))
         {
             console.log(className);
+            count = count + 1
         }
     }
+    console.log("Classes found: " + count);
 }
 else
 {
     console.log("Objective-C Runtime is not available!");
 }
-console.log("[*] Completed: Find Classes");

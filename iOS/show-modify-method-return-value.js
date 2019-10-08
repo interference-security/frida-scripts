9b863278acf723f4c2b4a544998e7d6d01c7f823
int3rf3r3nc3 <interference-security@users.noreply.github.com>
2019-10-08 13:25:48 +0530

    Simplified the script

--- iOS/show-modify-method-return-value.js
+++ iOS/show-modify-method-return-value.js
@@ -1,32 +1,21 @@
-if (ObjC.available)
+function show_modify_function_return_value(className_arg, funcName_arg)
 {
-    try
-    {
-        //Your class name here
-        var className = "YOUR_CLASS_NAME_HERE";
-        //Your function name here
-        var funcName = "YOUR_EXACT_FUNC_NAME_HERE";
-        var hook = eval('ObjC.classes.' + className + '["' + funcName + '"]');
-        Interceptor.attach(hook.implementation, {
-          onLeave: function(retval) {
-            console.log("[*] Class Name: " + className);
-            console.log("[*] Method Name: " + funcName);
-            console.log("\t[-] Type of return value: " + typeof retval);
-            //console.log(retval.toString());
-            console.log("\t[-] Return Value: " + retval);
-            //For modifying the return value
-            //newretval = ptr("0x0") //your new return value here
-            //retval.replace(newretval)
-            //console.log("\t[-] New Return Value: " + newretval)
-          }
-        });
-    }
-    catch(err)
-    {
-        console.log("[!] Exception2: " + err.message);
-    }
+    var className = className_arg;
+    var funcName = funcName_arg;
+    var hook = eval('ObjC.classes.' + className + '["' + funcName + '"]');
+    Interceptor.attach(hook.implementation, {
+      onLeave: function(retval) {
+        console.log("\n[*] Class Name: " + className);
+        console.log("[*] Method Name: " + funcName);
+        console.log("\t[-] Type of return value: " + typeof retval);
+        //console.log(retval.toString());
+        console.log("\t[-] Return Value: " + retval);
+        //For modifying the return value
+        newretval = ptr("0x0") //your new return value here
+        retval.replace(newretval)
+        console.log("\t[-] New Return Value: " + newretval)
+      }
+    });
 }
-else
-{
-    console.log("Objective-C Runtime is not available!");
-}
+//YOUR_CLASS_NAME_HERE and YOUR_EXACT_FUNC_NAME_HERE
+show_modify_function_return_value("YOUR_CLASS_NAME_HERE" ,"YOUR_EXACT_FUNC_NAME_HERE")

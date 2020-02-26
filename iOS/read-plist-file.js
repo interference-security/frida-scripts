9cb50055477f9b21b5a5e08c742dfcb6fa2d3dd7
int3rf3r3nc3 <interference-security@users.noreply.github.com>
2020-02-26 16:22:36 +0530

    Added read plist script

---
+++ iOS/read-plist-file.js
@@ -0,0 +1,8 @@
+function read_plist_file(file_location)
+{
+	var dict = ObjC.classes.NSMutableDictionary
+	console.log("[*] Read Plist File: " + file_location)
+	console.log("[*] File Contents:")
+	console.log(dict.alloc().initWithContentsOfFile_(file_location).toString())
+}
+read_plist_file("/path/to/file/filename.plist")//file location and path here

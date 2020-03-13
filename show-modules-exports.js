8ae44c84ef1505664d146f2fbdeeb633fc4decec
int3rf3r3nc3 <interference-security@users.noreply.github.com>
2020-03-13 14:04:05 +0530

    Removed repeating tasks

--- show-modules-exports.js
+++ show-modules-exports.js
@@ -1,13 +1,15 @@
 //Script to show modules and its exports. Works on both Android and iOS
 function show_modules_exports()
 {
+	var module_name_list = Process.enumerateModules();
 	for(var i=0 ; i<Process.enumerateModules().length ; i++)
 	{
-		var module_name = Process.enumerateModules()[i].name;
+		var module_name = module_name_list[i].name;
+		var module_export_list = Module.enumerateExports(module_name);
 		console.log("[*] " + module_name);
-		for(var j=0 ; j<Module.enumerateExports(module_name).length ; j++)
+		for(var j=0 ; j<module_export_list.length ; j++)
 		{
-			var module_export_name = Module.enumerateExports(module_name)[j].name;
+			var module_export_name = module_export_list[j].name;
 			console.log("\t[-] " + module_export_name);
 		}
 	}

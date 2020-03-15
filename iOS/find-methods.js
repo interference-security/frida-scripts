445d524f3a8ea3c3bc577319fc653ad672ce809e
int3rf3r3nc3 <interference-security@users.noreply.github.com>
2020-03-15 15:18:39 +0530

    Updated code with args and return results

--- iOS/find-methods.js
+++ iOS/find-methods.js
@@ -12,7 +12,13 @@ function run_show_classes_methods_of_app()
 			var methods = ObjC.classes[className].$ownMethods;
 			for (var i = 0; i < methods.length; i++)
 			{
-				console.log("\t[-] Method: "+methods[i]);
+				console.log("\t[-] Method: " + methods[i]);
+				try
+				{
+					console.log("\t\t[-] Arguments Type: " + ObjC.classes[className][methods[i]].argumentTypes);
+					console.log("\t\t[-] Return Type: " + ObjC.classes[className][methods[i]].returnType);
+				}
+				catch(err) {}
 			}
 		}
 	}

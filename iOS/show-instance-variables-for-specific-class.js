bb517377964d2bd31cad4741a457c8ef6c28a7a5
int3rf3r3nc3 <interference-security@users.noreply.github.com>
2020-03-15 17:38:52 +0530

    Added info

--- iOS/show-instance-variables-for-specific-class.js
+++ iOS/show-instance-variables-for-specific-class.js
@@ -1,3 +1,6 @@
+//Twitter: https://twitter.com/xploresec
+//GitHub: https://github.com/interference-security
+
 //Example: var instance = ObjC.chooseSync(ObjC.classes.SensitiveInformationDetailsVC)[0]
 //var instance = ObjC.chooseSync(ObjC.classes.CLASS_NAME_HERE)[0]
 //instance.$ivars

e8bfea56403245dfb0d1986f94b0a6352cc10fb9
int3rf3r3nc3 <interference-security@users.noreply.github.com>
2020-03-19 20:48:27 +0530

    Updated script with sysctl and enum info

--- iOS/replace-exported-method.js
+++ iOS/replace-exported-method.js
@@ -1,8 +1,18 @@
 //Twitter: https://twitter.com/xploresec
 //GitHub: https://github.com/interference-security
 
+//How to identify exports
+//Get a list of all modules: Process.enumerateModules()
+//Get a list of export for a module: Module.enumerateExports()
+
 //replace a function. In this example we are replacing ptrace
 var ptracePtr = Module.findExportByName(null, "ptrace"); //null can be replaced with libsystem_kernel.dylib which exports ptrace
 Interceptor.replace(ptracePtr, new NativeCallback(function () {
 	console.log("[*] Ptrace called and replaced")
+}, "int", []));
+
+//replace a function. In this example we are replacing sysctl
+var sysctlPtr = Module.findExportByName(null, "__sysctl"); //null can be replaced with libsystem_kernel.dylib which exports sysctl
+Interceptor.replace(sysctlPtr, new NativeCallback(function () {
+	console.log("[*] Sysctl called and replaced")
 }, "int", []));

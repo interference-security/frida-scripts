3bef184ac7918c3b58cd829d5b683368cd6e7156
int3rf3r3nc3 <interference-security@users.noreply.github.com>
2020-03-19 13:53:06 +0530

    Script to intercept NSLog and NSLogv calls

---
+++ iOS/intercept-nslog.js
@@ -0,0 +1,19 @@
+//Twitter: https://twitter.com/xploresec
+//GitHub: https://github.com/interference-security
+//Note: This interception does not print the string interpolation (or formatting) values such as %s, %ld, %f, %a. Still working on a fix.
+
+Interceptor.attach(Module.findExportByName("Foundation", "NSLog"), {
+	onEnter: function(args) {
+		console.log("[*] NSLog was called")
+		console.log(ObjC.Object(ptr(args[0])))
+		//console.log((ObjC.Object(ptr(args[0]))).toString())
+		//console.log((ObjC.Object(args[0])).toString())
+	}
+});
+//As per the Apple documentation NSLog calls NSLogv in the background but for some reason it is not working. Still working on a fix.
+Interceptor.attach(Module.findExportByName("Foundation", "NSLogv"), {
+	onEnter: function(args) {
+		console.log("[*] NSLogv was called")
+		console.log(ObjC.Object(ptr(args[0])))
+	}
+});

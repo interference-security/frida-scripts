568746a3b215fb7a1e42e3e99a55d5208ae5bbef
int3rf3r3nc3 <interference-security@users.noreply.github.com>
2020-03-20 15:32:56 +0530

    Converted to function

--- show-module-exported-functions.js
+++ show-module-exported-functions.js
@@ -1,7 +1,15 @@
-//Update module name in below line
-var module_export_list = Module.enumerateExports("libsqlite3.dylib")
-for(var j=0 ; j<module_export_list.length ; j++)
+//Twitter: https://twitter.com/xploresec
+//GitHub: https://github.com/interference-security
+function show_specific_module_exported_functions(modulesName)
 {
-	if(module_export_list[j].type == "function")
-		console.log(module_export_list[j].name)
+	//var modulesName = "libsqlite3.dylib";
+	var module_export_list = Module.enumerateExports(modulesName)
+	for(var j=0 ; j<module_export_list.length ; j++)
+	{
+		if(module_export_list[j].type == "function")
+			console.log(module_export_list[j].name)
+	}
 }
+
+//Update module name in below line
+show_specific_module_exported_functions("MODULE_NAME_HERE")

baf367674d530331e8707d5f5c6ae082ae9b24ac
int3rf3r3nc3 <interference-security@users.noreply.github.com>
2020-03-20 15:04:58 +0530

    Converted to function

--- iOS/show-argument-type-count-and-return-value-type.js
+++ iOS/show-argument-type-count-and-return-value-type.js
@@ -1,10 +1,16 @@
 //Twitter: https://twitter.com/xploresec
 //GitHub: https://github.com/interference-security
 
-console.warn("[*] Show number and type of arguments for the function")
-//ObjC.classes["DamnVulnerableAppUtilities"]["+ showAlertForJailbreakTestIsJailbroken:"].argumentTypes
-ObjC.classes["CLASS_NAME_HERE"]["FUNCTION_NAME_HERE"].argumentTypes
+function show_method_args_return_type(className, methodName)
+{
+  console.warn("[*] Show number and type of arguments for the function")
+  //ObjC.classes["DamnVulnerableAppUtilities"]["+ showAlertForJailbreakTestIsJailbroken:"].argumentTypes
+  ObjC.classes[className][methodName].argumentTypes
 
-console.warn("[*] Show type of return value for the function")
-//ObjC.classes["JailbreakDetectionVC"]["- isJailbroken"].returnType
-ObjC.classes["CLASS_NAME_HERE"]["FUNCTION_NAME_HERE"].returnType
+  console.warn("[*] Show type of return value for the function")
+  //ObjC.classes["JailbreakDetectionVC"]["- isJailbroken"].returnType
+  ObjC.classes[className][methodName].returnType
+}
+
+//Your class name and method name here
+show_method_args_return_type("CLASS_NAME_HERE", "METHOD_NAME_HERE")

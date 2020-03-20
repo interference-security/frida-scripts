0c560f0677933b6f85d7ac77621b4e190070e462
int3rf3r3nc3 <interference-security@users.noreply.github.com>
2020-03-20 14:51:35 +0530

    Bug fix and removed eval use

--- iOS/show-modify-function-arguments.js
+++ iOS/show-modify-function-arguments.js
@@ -1,21 +1,23 @@
 //Twitter: https://twitter.com/xploresec
 //GitHub: https://github.com/interference-security
 
-//Your class name here
-var className = "YOUR_NAME_HERE";
-//Your function name here
-var funcName = "YOUR_FUNCTION_NAME_HERE";
-var hook = eval('ObjC.classes.' + className + '["' + funcName + '"]');
-Interceptor.attach(hook.implementation, {
-  onEnter: function(args) {
-    // args[0] is self
-    // args[1] is selector (SEL "sendMessageWithText:")
-    // args[2] holds the first function argument, an NSString
-    console.log("\n[*] Detected call to: " + className + " -> " + funcName);
-    console.log("\t[-] Argument Value: "+args[2]);
-    //your new argument value here
-    var newargval = ptr("0x0")
-    args[2] = newargval
-    console.log("\t[-] New Argument Value: " + args[2])
-  }
-});
+function show_modify_function_args(className, funcName)
+{
+  var hook = ObjC.classes[className][funcName];
+  Interceptor.attach(hook.implementation, {
+    onEnter: function(args) {
+      // args[0] is self
+      // args[1] is selector (SEL "sendMessageWithText:")
+      // args[2] holds the first function argument, an NSString
+      console.log("\n[*] Detected call to: " + className + " -> " + funcName);
+      console.log("\t[-] Argument Value: "+args[2]);
+      //your new argument value here
+      var newargval = ptr("0x0")
+      args[2] = newargval
+      console.log("\t[-] New Argument Value: " + args[2])
+    }
+  });
+}
+
+//Your class name and function name here
+show_modify_function_args("className", "funcName")

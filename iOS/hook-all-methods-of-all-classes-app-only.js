4eae8fd90c23c0ab2cc4faad0ac0efd1effa6c37
int3rf3r3nc3 <interference-security@users.noreply.github.com>
2020-03-20 14:43:55 +0530

    Bug fix and removed eval use

--- iOS/hook-all-methods-of-all-classes-app-only.js
+++ iOS/hook-all-methods-of-all-classes-app-only.js
@@ -7,7 +7,7 @@ function get_timestamp()
 
 function hook_class_method(class_name, method_name)
 {
-	var hook = eval('ObjC.classes.'+class_name+'["'+method_name+'"]');
+	var hook = ObjC.classes[class_name][method_name];
 		Interceptor.attach(hook.implementation, {
 			onEnter: function(args) {
 			console.log("[*] [" + get_timestamp() + " ] Detected call to: " + class_name + " -> " + method_name);

885d9d190e5cbbd19edfc43d939058b43cb8c2a5
int3rf3r3nc3 <interference-security@users.noreply.github.com>
2020-03-20 15:02:59 +0530

    Bug fix

--- iOS/show-argument-type-count-and-return-value-type.js
+++ iOS/show-argument-type-count-and-return-value-type.js
@@ -2,9 +2,9 @@
 //GitHub: https://github.com/interference-security
 
 console.warn("[*] Show number and type of arguments for the function")
-//ObjC.classes.DamnVulnerableAppUtilities['+ showAlertForJailbreakTestIsJailbroken:'].argumentTypes
-ObjC.classes.CLASS_NAME_HERE["FUNCTION_NAME_HERE"].argumentTypes
+//ObjC.classes["DamnVulnerableAppUtilities"]["+ showAlertForJailbreakTestIsJailbroken:"].argumentTypes
+ObjC.classes["CLASS_NAME_HERE"]["FUNCTION_NAME_HERE"].argumentTypes
 
 console.warn("[*] Show type of return value for the function")
-//ObjC.classes.JailbreakDetectionVC['- isJailbroken'].returnType
-ObjC.classes.CLASS_NAME_HERE["FUNCTION_NAME_HERE"].returnType
+//ObjC.classes["JailbreakDetectionVC"]["- isJailbroken"].returnType
+ObjC.classes["CLASS_NAME_HERE"]["FUNCTION_NAME_HERE"].returnType

7dd2a739d6de922d32df17d6cc28b4097ea46ad3
int3rf3r3nc3 <interference-security@users.noreply.github.com>
2020-07-22 19:36:28 -0400

    Hook method of class

---
+++ android/hook-method-of-class.js
@@ -0,0 +1,9 @@
+Java.perform(function() {
+  //enter class name here: example android.security.keystore.KeyGenParameterSpec$Builder
+	//class inside a class is defined using CLASS_NAME$SUB_CLASS_NAME
+	var class_name = Java.use("android.security.keystore.KeyGenParameterSpec$Builder");
+  //replace FUNC_NAME_HERE with method name you want to hook and remove arg1 or add more if the function has arguments
+	class_name.FUNC_NAME_HERE.implementation = function (arg1) {
+		console.log("[*] CLASS_NAME:FUNC_NAME was called");
+	}
+});

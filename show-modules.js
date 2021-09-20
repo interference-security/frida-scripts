6fa4267ac4acb0e8710811600f64810dc79663b1
Abhineet Jayaraj <interference-security@users.noreply.github.com>
2021-09-20 16:19:17 -0400

    Added show-modules script

---
+++ show-modules.js
@@ -0,0 +1,13 @@
+function show_modules()
+{
+	var count = 0;
+	var module_name_list = Process.enumerateModules();
+	for(var i=0 ; i<Process.enumerateModules().length ; i++)
+	{
+		var module_name = module_name_list[i].name;
+		console.log("[*] " + module_name);
+		count = count + 1;
+	}
+	console.log("\nFound Modules: " + count + "\n");
+}
+setImmediate(show_modules)
